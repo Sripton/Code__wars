2fca88847dff4b03e0042c4f0ac18ce2ad8c582d
Elmar Erzikhanov <elmarerzikhanov@MacBook-Pro-Elmar.local>
2024-05-18 19:55:00 +0300

    Diamond new code

--- Q8/diamond.js
+++ Q8/diamond.js
@@ -47,7 +47,7 @@
 //   ) {
 //     str += " ".repeat(tabsArray[i]) + "*".repeat(starsArray[j]) + "\n";
 //   }
-//   return str; 
+//   return str;
 // };
 // console.log(diamond(7));
 //    *
@@ -57,3 +57,80 @@
 //  *****
 //   ***
 //    *
+
+// Решение 3
+// const diamond = (n) => {
+//   if (n <= 0 && n % 2 === 0) {
+//     return null;
+//   }
+//   const midle = parseInt(n + 1) / 2;
+//   let i = 1;
+//   let str = "";
+//   while (i <= n) {
+//     if (i <= midle) {
+//       str += " ".repeat(midle - i) + "*".repeat(2 * i - 1) + "\n";
+//     } else {
+//       str +=
+//         " ".repeat(Math.abs(midle - i)) + "*".repeat(2 * (n - i) + 1) + "\n";
+//     }
+//     i++;
+//   }
+//   return str;
+// };
+// console.log(diamond(7));
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+
+// Решение 4
+// const diamond = (n) => {
+//     if(n <= 0 && n % 2 === 0){
+//         return null;
+//     }
+//     let str = '';
+//     for(let i = 0; i < n; i++){
+//         const helper = Math.abs((n-2*i -1)/2);
+//         str += ' '.repeat(helper);
+//         if(i <= Math.floor(n/2)){
+//             str += '*'.repeat(2 * i +1)  + '\n'
+//         }else {
+//             str += '*'.repeat(2 * (n-i)-1) + "\n";
+//         }
+//     }
+//     return str;
+// }
+
+//  console.log(diamond(7));
+
+//   *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+
+// Решение 5
+// const diamond = (n) => {
+//   if (n <= 0 && n % 2 === 0) {
+//     return null;
+//   }
+//   const midle = Math.floor(n/2);
+//   return Array.apply(null, { length: n }).map((element, index) => {
+//     const identefiction = Math.abs(index - midle);
+//     const diamondList = n - identefiction *2;
+//     return Array(identefiction+1).join(' ') + Array(diamondList+1).join("*")
+//   }).join('\n');
+// return midle
+// };
+//   console.log(diamond(5));
+
+// //    *
+// //   ***
+// //  *****
+// //   ***
+// //    *
