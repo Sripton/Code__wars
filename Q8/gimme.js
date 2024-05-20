4acc2da2cef96305cae527f2b4d68dc79ff48792
Elmar Erzikhanov <elmarerzikhanov@MacBook-Pro-Elmar.local>
2024-05-20 22:26:02 +0300

    Gimme update

--- Q8/gimme.js
+++ Q8/gimme.js
@@ -15,25 +15,62 @@
 // console.log(gimme([2, 11, 7])); // 2
 
 //Решение 2
-const gimme = (triplet) => {
-  const result = [...triplet];
-  for (let i = 0; i < triplet.length; i++) {
-    for (let j = result.length - 1; j > i; j--) {
-      if (result[j] < result[i]) {
-        let temp = result[i];
-        result[i] = result[j];
-        result[j] = temp;
-      }
-    }
-  }
-  let elem = result[1];
- for(let i = 0; i < result.length; i++){
-    if(elem > result[i-1] && elem < result[i+1]){
-        return result.indexOf(elem)
-    }
- }
-};
-console.log(gimme([23, 11, 5])); //  1
-console.log(gimme([2, 11, 5])); //  2
-console.log(gimme([23, 11, 76])); // 0
-console.log(gimme([2, 11, 7])); // 2
+// const gimme = (triplet) => {
+//   const result = [...triplet];
+//   for (let i = 0; i < triplet.length; i++) {
+//     for (let j = result.length - 1; j > i; j--) {
+//       if (result[j] < result[i]) {
+//         let temp = result[i];
+//         result[i] = result[j];
+//         result[j] = temp;
+//       }
+//     }
+//   }
+//   let elem = result[1];
+//  for(let i = 0; i < result.length; i++){
+//     if(elem > result[i-1] && elem < result[i+1]){
+//         return result.indexOf(elem)
+//     }
+//  }
+// };
+// console.log(gimme([23, 11, 5])); //  1
+// console.log(gimme([2, 11, 5])); //  2
+// console.log(gimme([23, 11, 76])); // 0
+// console.log(gimme([2, 11, 7])); // 2
+
+//Решение  3
+// const gimme = (triplet) => {
+//   const array = [...triplet];
+//   array.sort((a,b) => a - b);
+//   const min = array[0], max = array[array.length-1];
+//   return triplet.map((elem, index) => elem > min && elem < max ? index : null ).filter((elem) => elem !== null).pop();
+// };
+// console.log(gimme([23, 11, 5])); //  1
+// console.log(gimme([2, 11, 5])); //  2
+// console.log(gimme([23, 11, 76])); // 0
+// console.log(gimme([2, 11, 7])); // 2
+
+//Решение  4
+// const gimme = (triplet) => {
+//   return triplet.indexOf([...triplet].sort((a,b) => a - b)[1]);
+// }
+// console.log(gimme([23, 11, 5])); //  1
+// console.log(gimme([2, 11, 5])); //  2
+// console.log(gimme([23, 11, 76])); // 0
+// console.log(gimme([2, 11, 7])); // 2
+
+//Решение  5
+// const gimme = (triplet) => {
+//   return triplet
+//     .map((elem, index) => {
+//       if (elem > Math.min(...triplet) && elem < Math.max(...triplet)) {
+//         return index;
+//       }
+//     })
+//     .filter((elem) => elem !== undefined).pop();
+// };
+
+// console.log(gimme([23, 11, 5])); //  1
+// console.log(gimme([2, 11, 5])); //  2
+// console.log(gimme([23, 11, 76])); // 0
+// console.log(gimme([2, 11, 7])); // 2
